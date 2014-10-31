6d606317758b51260370e75042ea8634a66ff551
Anthony May <anthonymay@gmail.com>
2014-10-31 10:46:34 -0400

    Split off model. Added request handling code.
    The model was added to it’s own file and model directory for more easy
    refactoring later. The request handling code has replaced the mockups.
    Also added connection pooling to the database definition.

--- app.rb
+++ app.rb
@@ -1,43 +1,63 @@
 #!/usr/bin/env ruby
+# This is a simple REST API for events created as a programming task.
+# Data is accepted and transmitted as JSON.
+#
+# Events are defined by a name and latitude/longitude coordinate in
+# a JSON property called 'data.' They are inserted with a unique id
+# and individually accessed via that id.
+#
+# A real API should have a security mechanism like OAuth and force the use
+# of https.
+#
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'model/event'
 
 configure { set :server, :puma }
 
-class Event < ActiveRecord::Base
-end
-
 class EventApp < Sinatra::Base
     before do
+        # This is executed before the individual requests
         content_type :json
+        method = request.request_method
+        if method == 'POST' or method == 'PUT'
+            @data = JSON.parse request.body.read
+        end
     end
     
     post '/events' do
-        p "Make new event"
+        # Add a new event.
+        Event.create(data: @data['data'])
     end
 
     get '/events/?' do
-        # List all events
-        @events = Event.all
-        @events.to_json
+        # Returns a list of all events in the database.
+        events = Event.all
+        events.to_json
     end
 
     delete '/events' do
-        p "Delete all events"
+        # Delete all events.
+        Event.destroy_all
     end
 
     get '/events/:id' do
-        # Show event with :id
-        @event = Event.find_by_id(params[:id])
-        @event.to_json
+        # Return a particular event identified by :id
+        event = Event.find_by_id(params[:id])
+        event.to_json
     end
 
     put '/events/:id' do
-        p "Update event :id"
+        # Update an existing event.
+        event = Event.find_by_id(params[:id])
+        event.data = @data['data']
+        event.save
     end
 
     delete '/events/:id' do
-        p "Delete event :id"
+        # Delete event identified by :id
+        event = Event.find_by_id(params[:id])
+        event.destroy
     end
 end
---
+++ model/event.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# This is the ActiveRecord representation of the events schema in the database.
+
+class Event < ActiveRecord::Base
+end
