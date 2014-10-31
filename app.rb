b12c14c3d648c081d904515a2b7676d61903aeb7
Anthony May <anthonymay@gmail.com>
2014-10-31 14:04:38 -0400

    Exception handling and db connection closing
    Fixed a bug where DB connections which are implicitly opened are not
    implicitly closed. This lead to the connection pool becoming exhausted
    and leading to connection exceptions. Also added other exception
    handling for non existent resources and a catch-all exception handler
    for all other exceptions.

--- app.rb
+++ app.rb
@@ -14,11 +14,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'model/event'
 
-configure { set :server, :puma }
+configure { set :server, :puma } 
 
 class EventApp < Sinatra::Base
     before do
-        # This is executed before the individual requests
+        # Set the content_type for all requests to JSON and parse data for POST/PUT
         content_type :json
         method = request.request_method
         if method == 'POST' or method == 'PUT'
@@ -59,5 +59,35 @@ class EventApp < Sinatra::Base
         # Delete event identified by :id
         event = Event.find_by_id(params[:id])
         event.destroy
+    end
+    
+    after do
+        # ActiveRecord is nice enough to implicitly open a connection but
+        # it does not implicitly relinquish them in this case.
+        close_connections
+    end
+    
+    not_found do
+        # Called when a URI is requested that doesn't exist.
+        status 404
+        error = {}
+        error['status'] = 'InvalidUri' 
+        error['message'] = 'The requested URI does not exist on the server.'
+        error.to_json
+    end
+    
+    error do
+        # Called when an exception occurs in a route or filter.
+        close_connections
+        status 500
+        error = {}
+        error['status'] = 'InternalError' 
+        error['message'] = 'There has been an unhandled exception process the request.'
+        error.to_json
+    end
+    
+    def close_connections
+        # Close all connections held by this thread.
+        ActiveRecord::Base.clear_active_connections!
     end
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
  
 require './app'
+
+# Start puma with the following default host and port
+#\ -o localhost -p 8999
+
 run EventApp
